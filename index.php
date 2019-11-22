61f25089ba92f4ee330b7d16aa799cc16f2994af
Matthew Obert <matt@hotwebmatter.com>
2019-11-22 16:04:59 +0000

    Initial commit of OO PHP exercises

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+
+require_once("Car.php");
+
+ // let's build a car
+ $myCar = new Car();
+ 
+ $myCar->setMake("Dodge");
+ $myCar->setModel("Dart");
+ $myCar->setYear(1968);
+ $myCar->setPrice(2500.37);
+ $myCar->setUsed(true);
+ $myCar->setMileage(500000);
+ $myCar->setColor("Green");
+ echo("Make: ");
+ echo($myCar->getMake());
+ echo("<br />Model: " . $myCar->getModel());
+ echo("<br />Year: " . $myCar->getYear());
+ echo("<br />Price: $" . $myCar->getPrice());
+ echo("<br />Used: " . $myCar->getUsed());
+ echo("<br />Mileage: " . $myCar->getMileage());
+ echo("<br />Color: " . $myCar->getColor());
+
+ var_dump($myCar);
