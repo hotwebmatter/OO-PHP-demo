df754fea8b89989147be3122963adaadcc677585
Matthew Obert <matt@hotwebmatter.com>
2019-11-22 17:03:56 +0000

    Remember to commit the Car class

---
+++ src/Vehicles/Car.php
@@ -0,0 +1,82 @@
+<?php 
+
+/**
+ * Our Car class definition
+ */
+ 
+namespace Vehicles;
+
+class Car {
+    
+ 	private $make;
+ 	private $model;
+ 	private $year;
+ 	private $price;
+ 	private $used;
+ 	private $mileage;
+ 	private $color;
+ 	
+ 	public function __construct() {
+ 		echo("Constructor called<br />");
+ 	}
+ 	
+ 	public function setMake(String $n) {
+ 		$this->make = $n;
+ 	}
+ 
+ 	public function getMake() {
+ 		return $this->make;
+ 	}
+
+ 	public function setModel(String $n) {
+ 		$this->model = $n;
+ 	}
+ 
+ 	public function getModel() {
+ 		return $this->model;
+ 	}
+
+ 	public function setYear(Int $n) {
+ 		$this->year = $n;
+ 	}
+ 
+ 	public function getYear() {
+ 		return $this->year;
+ 	}
+
+ 	public function setPrice(Float $n) {
+ 		$this->price = $n;
+ 	}
+ 
+ 	public function getPrice() {
+ 		return $this->price;
+ 	}
+
+ 	public function setUsed(Bool $n) {
+ 		$this->used = $n;
+ 	}
+ 
+ 	public function getUsed() {
+ 		if ($this->used) {
+ 			return "TRUE!";
+ 		}
+ 		return "FALSE!";
+ 	}
+
+ 	public function setMileage(Float $n) {
+ 		$this->mileage = $n;
+ 	}
+ 
+ 	public function getMileage() {
+ 		return $this->mileage;
+ 	}
+
+ 	public function setColor(String $n) {
+ 		$this->color = $n;
+ 	}
+ 
+ 	public function getColor() {
+ 		return $this->color;
+ 	}
+ 	
+}
